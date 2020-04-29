360e35edf46b871900a9d14d703753187e6aed56
trantbatey <trantbatey@gmail.com>
2020-04-29 10:21:01 -0500

    Added User Input XML files.

--- src/main/java/com/codeup/TOCBuilderApplication.java
+++ src/main/java/com/codeup/TOCBuilderApplication.java
@@ -18,6 +18,6 @@ public class TOCBuilderApplication {
 
     public static void main(String[] args) {
         XMLHandler XMLHandler = new TOCHandler();
-        XMLHandler.parseXMLFile(XMLHandler, "data/MySQLIntroTOC.xml");
+        XMLHandler.parseXMLFile(XMLHandler, "data/UserInput/UserInputTOC.xml");
     }
 }

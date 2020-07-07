df0423672d0d796bc022a4a115f536c7749783e1
trantbatey <trantbatey@gmail.com>
2020-07-07 11:16:41 -0500

    Completed the DOM Events presentation.

--- src/main/java/com/codeup/SlideBuilderApplication.java
+++ src/main/java/com/codeup/SlideBuilderApplication.java
@@ -4,6 +4,6 @@ public class SlideBuilderApplication {
 
     public static void main(String[] args) {
         XMLHandler XMLHandler = new XMLHandler();
-        com.codeup.XMLHandler.parseXMLFile(XMLHandler, "data/03_JavaScriptI/10_BomDom/BomDom06DomLocating.xml");
+        com.codeup.XMLHandler.parseXMLFile(XMLHandler, "data/03_JavaScriptI/10_BomDom/BomDom10EvtReview.xml");
     }
 }

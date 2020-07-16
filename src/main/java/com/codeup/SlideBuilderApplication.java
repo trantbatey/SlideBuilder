020345f10706294bb8d3040531b82de13b61c3bf
trantbatey <trantbatey@gmail.com>
2020-07-16 14:09:48 -0500

    Added HTML Forms.

--- src/main/java/com/codeup/SlideBuilderApplication.java
+++ src/main/java/com/codeup/SlideBuilderApplication.java
@@ -4,6 +4,6 @@ public class SlideBuilderApplication {
 
     public static void main(String[] args) {
         XMLHandler XMLHandler = new XMLHandler();
-        com.codeup.XMLHandler.parseXMLFile(XMLHandler, "data/02_HTMLandCSS/Html03Elements.xml");
+        com.codeup.XMLHandler.parseXMLFile(XMLHandler, "data/02_HTMLandCSS/Html04Forms.xml");
     }
 }
